26f38285f543283822737e6539a47befd8c747ac
Kubilay Bozak <kubilay.bozak@alkanlar.com>
2022-01-28 16:23:05 +0300

    React Router Medium

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Router Hook kullanım Örneği
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Projenin reposunu 
           <code className={styles.code}>pages/index.js</code>
         </p>
 
