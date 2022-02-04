811e0d32e70d774028cee4e32bbe56308983eb71
Kubilay Bozak <kubilay.bozak@alkanlar.com>
2022-02-04 08:58:12 +0300

    V2
    V2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className={styles.container}>
       <Head>
@@ -12,43 +15,23 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Router Hook kullanım Örneği
-        </h1>
+        <h1 className={styles.title}>Router Hook Ana Sayfamız</h1>
 
         <p className={styles.description}>
-          Projenin reposunu 
-          <code className={styles.code}>pages/index.js</code>
+          Şuan Aktif olan Path :
+          <code className={styles.code}>localost:3000{router.asPath}</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div>
+            <Link href="/pictures" className={styles.card}>
+              <h2>RESİMLER İÇİN TIKLAYIN 1 &rarr;</h2>
+            </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <Link href="/text" className={styles.card}>
+              <h2>TEXTLER İÇİN TIKLAYIN 2 &rarr;</h2>
+            </Link>
+          </div>
         </div>
       </main>
 
@@ -58,12 +41,34 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
+  );
+}
+
+/*
+const Link = ({ children, href }) => {
+  const router = useRouter()
+  return (
+    <a
+      href="#"
+      onClick={(e) => {
+        e.preventDefault()
+        // typically you want to use `next/link` for this usecase
+        // but this example shows how you can also access the router
+        // and use it manually
+        router.push(href)
+      }}
+    >
+      {children}
+
+
+    </a>
   )
 }
+*/
